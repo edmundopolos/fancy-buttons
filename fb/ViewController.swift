5217487af6bf24deb7afde7014ff0920a1b5b3b1
edmund <eddyigene@gmail.com>
2022-08-24 19:02:48 +0100

    Initial Commit

---
+++ fb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fb
+//
+//  Created by Edmund on 24/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
